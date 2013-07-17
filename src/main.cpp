f2e94f6a87dbc2e1371fad30666cffa1f5f60e45
kidanger <kidanger@gmail.com>
2013-07-17 23:07:38 +0200

    fix space format in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 
 int main(int argc, const char* argv[])
 {
-    const char* filename = "data/main.lua";
-    if (argc == 2) {
-        filename = argv[1];
-    }
+	const char* filename = "data/main.lua";
+	if (argc == 2) {
+		filename = argv[1];
+	}
 
-    Engine e(filename, 42);
-    e.loop();
-    return 0;
+	Engine e(filename, 60);
+	e.loop();
+	return 0;
 }
