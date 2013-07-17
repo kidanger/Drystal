0bbdc0694749c53b58e9a44ce04ba16c5fdf69a2
kidanger <kidanger@gmail.com>
2013-07-17 16:54:09 +0200

    Improve tff test

--- tests/ttf/test_draw.lua
+++ tests/ttf/test_draw.lua
@@ -2,29 +2,38 @@ require 'data/drystal'
 
 function init()
 	resize(600, 400)
-
-	set_font('data/arial.ttf', 16)
 end
 
-function update_text(text)
-	if surf then free_surface(surf) end
+function update()
+end
 
-	set_color(0,0,0)
+function draw_text(text, textcolor, fontname, size, y, drawcolor)
+	drawcolor = drawcolor or {255,255,255}
+
+	set_font(fontname, size)
+	set_color(textcolor)
 	surf = text_surface(text)
+
+	local w, h = surface_size(surf)
+	local sprite = {x=0, y=0, w=w, h=h}
+
+	set_color(drawcolor)
 	draw_from(surf)
+	draw_sprite(sprite, 0, y)
+
+	free_surface(surf)
+	return y+h
 end
 
-function update(dt)
-	update_text(tostring(dt))
-end
 
 function draw()
 	set_color(255,255,255)
 	draw_background()
 
-	local w, h = surface_size(surf)
-	local sprite = {x=0, y=0, w=w, h=h}
-	draw_sprite(sprite, 0, 0)
+	local y = 0
+	y = draw_text('Red Arial 16', {255,0,0}, 'data/arial.ttf', 16, y, {255,255,255})
+	y = draw_text('Black Arial 20', {0,0,0}, 'data/arial.ttf', 20, y, {255,255,255})
+	y = draw_text('Black Arial 24', {255,255,255}, 'data/arial.ttf', 24, y, {0,0,0})
 
 	flip()
 end
