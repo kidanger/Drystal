84783285bb06b21a0d6f9e04d2f48fab0ca84b70
kidanger <angerj.dev@gmail.com>
2013-08-29 19:53:10 +0200

    Add storage extensions (uses file on native and localStorage on web)

---
+++ tests/storage/main.lua
@@ -0,0 +1,20 @@
+local storage = require 'storage'
+
+local data = storage.load('test_storage')
+print(data)
+
+print('saving...')
+storage.save('test_storage', {text='blabla'})
+print('saved')
+
+data = storage.load('test_storage')
+
+if not data then
+	print('data not loaded')
+else
+	print(data.text)
+end
+
+function init()
+	engine_stop()
+end
