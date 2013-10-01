81ae297909b5c7eefa373af9f3f675c756b37e8f
kidanger <angerj.dev@gmail.com>
2013-10-01 22:43:58 +0200

    Protect against invalid raycasts

--- extensions/physic/main.cpp
+++ extensions/physic/main.cpp
@@ -212,7 +212,9 @@ int raycast(lua_State* L)
 	CustomRayCastCallback callback;
 	callback.L = L;
 	callback.ref = callback_ref;
-	world->RayCast(&callback, b2Vec2(x1, y1), b2Vec2(x2, y2));
+	if (x1 != x2 and y1 != y2) {
+		world->RayCast(&callback, b2Vec2(x1, y1), b2Vec2(x2, y2));
+	}
 	luaL_unref(L, LUA_REGISTRYINDEX, callback_ref);
 
 	if (callback.fixture) {
