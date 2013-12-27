46de0e9b3585809029e03c353bb77ee8f63a3851
kidanger <angerj.dev@gmail.com>
2013-12-27 20:36:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,18 +10,20 @@ BIN=$PREFIX/usr/bin
 LIB=$PREFIX/usr/lib64
 SHARE=$PREFIX/usr/share/drystal
 
+BUILD=build-native-release
+
 test -d $BIN || mkdir -p $BIN
 test -d $LIB || mkdir -p $LIB
 test -d $SHARE || mkdir -p $SHARE
 
-cp build-native/src/drystal $BIN/
-cp build-native/external/liblua-drystal.so $LIB/
+cp $BUILD/src/drystal $BIN/
+cp $BUILD/external/liblua-drystal.so $LIB/
 
-for ext in `ls build-native/extensions`
+for ext in `ls $BUILD/extensions`
 do
-	test -d build-native/extensions/${ext} \
-	-a -f build-native/extensions/${ext}/lib${ext}.so &&
-		cp build-native/extensions/$ext/lib${ext}.so $SHARE/${ext}.so
+	test -d $BUILD/extensions/${ext} \
+	-a -f $BUILD/extensions/${ext}/lib${ext}.so &&
+		cp $BUILD/extensions/$ext/lib${ext}.so $SHARE/${ext}.so
 done
 
 for lua in `ls data/*.lua`
@@ -32,10 +34,10 @@ done
 echo "#!/bin/sh -x" > remove.sh
 echo "rm $BIN/drystal" >>remove.sh
 echo "rm $LIB/liblua-drystal.so" >>remove.sh
-for ext in `ls build-native/extensions`
+for ext in `ls $BUILD/extensions`
 do
-	test -d build-native/extensions/${ext} \
-	-a -f build-native/extensions/${ext}/lib${ext}.so &&
+	test -d $BUILD/extensions/${ext} \
+	-a -f $BUILD/extensions/${ext}/lib${ext}.so &&
 		echo "rm $SHARE/${ext}.so" >>remove.sh
 done
 for lua in `ls data/*.lua`
