2cdba4f4c52b56eb0b05d71049b8841ee0d288d4
statler <statler@statler.fr>
2014-01-02 21:41:12 +0100

    add more perf tests

--- tests/display/perf.lua
+++ tests/display/perf.lua
@@ -1,35 +1,41 @@
 local drystal = require 'drystal'
+local tt = require 'truetype'
 
 local W, H = 600, 400
 local target = 16
 local number = 300
 
-function drystal.init()
-	drystal.resize(W, H)
-end
-
+local draw_triangle = {name='draw_triangle'}
+local draw_sprite_simple = {name='draw_sprite_simple'}
+local draw_sprite_rotated = {name='draw_sprite_rotated'}
+local draw_sprite_resized = {name='draw_sprite_resized'}
+local draw_font_nocolor = {name='draw_font_nocolor'}
+local draw_font_color = {name='draw_font_color'}
+local state = {}
+local states = { draw_triangle, draw_sprite_simple, draw_sprite_rotated, draw_sprite_resized, draw_font_nocolor, draw_font_color }
+local current_state = 1
+local number = 0
 local tick = 0
-function drystal.update(dt)
-	if tick > 100 then
-		if dt > target then
-			number = number - 5
-		else
-			number = number + 30
-		end
-	end
-	tick = tick + 1
-	if tick % 60 == 0 then
-		print(number, dt - target)
-	end
+local random = math.random
+local sprite = {x=0, y=0, w=22, h=33}
+local spritesheet = nil
+local font = nil
+local font_big = nil
+
+function set_state(s)
+    state = s
+    s.max = 0
+    if state.init then
+        state:init()
+    end
 end
 
-function drystal.draw()
+function draw_triangle:draw()
 	drystal.set_color(0, 0, 0)
 	drystal.draw_background()
 
 	drystal.set_alpha(255)
 	drystal.set_color(255, 0, 0)
-	local random = math.random
 	for i = 1, number do
 		local x = random(W)
 		local y = random(H)
@@ -41,8 +47,133 @@ function drystal.draw()
 	end
 end
 
+function draw_sprite_simple:draw()
+	drystal.set_color(255, 255, 255)
+	drystal.set_alpha(255)
+	drystal.draw_background()
+
+	local random = math.random
+	for i = 1, number do
+		local x = random(W)
+		local y = random(H)
+		drystal.draw_sprite(sprite, x, y)
+	end
+end
+
+function draw_sprite_rotated:draw()
+	drystal.set_color(255, 255, 255)
+	drystal.set_alpha(255)
+	drystal.draw_background()
+
+	local random = math.random
+	for i = 1, number do
+		local x = random(W)
+		local y = random(H)
+        local r = random(math.pi)
+		drystal.draw_sprite_rotated(sprite, x, y, r)
+	end
+end
+
+function draw_sprite_resized:draw()
+	drystal.set_color(255, 255, 255)
+	drystal.set_alpha(255)
+	drystal.draw_background()
+
+	local random = math.random
+	for i = 1, number do
+		local x = random(W)
+		local y = random(H)
+		local w = sprite.w*(random()+random(0,1))
+		local h = sprite.h*(random()+random(0,1))
+		drystal.draw_sprite_resized(sprite, x, y, w, h)
+	end
+end
+
+function highlight(text, pos)
+	return text:sub(0, pos-1) .. '{big|'.. text:sub(pos, pos) .. '}' .. text:sub(pos+1, #text)
+end
+
+function draw_font_nocolor:init()
+	tt.use(font)
+	tt.use_color(false)
+end
+
+function draw_font_nocolor:draw()
+	drystal.set_alpha(255)
+	drystal.set_color(200, 200, 200)
+	drystal.draw_background()
+
+	drystal.set_color(255, 0, 0)
+	local text = 'abd {r0|big|b150|bla} {small|test} {big|50%|defghi}'
+	for i = 1, number do
+        local x = random(W)
+        local y = random(H)
+	    tt.draw_align(text, x, y, 'center')
+    end
+end
+
+function draw_font_color:init()
+	tt.use(font)
+	tt.use_color(true)
+end
+
+function draw_font_color:draw()
+	drystal.set_alpha(255)
+	drystal.set_color(200, 200, 200)
+	drystal.draw_background()
+
+	drystal.set_color(255, 0, 0)
+	local text = 'abd {r0|big|b150|bla} {small|test} {big|50%|defghi}'
+	for i = 1, number do
+        local x = random(W)
+        local y = random(H)
+	    tt.draw_align(text, x, y, 'center')
+    end
+end
+
+function drystal.init()
+	drystal.resize(W, H)
+	font = tt.load('arial.ttf', 16)
+    spritesheet = drystal.load_surface('spritesheet.png')
+    drystal.draw_from(spritesheet)
+    set_state(states[current_state])
+
+    print('name                max')
+end
+
+function drystal.update(dt)
+	if tick > 100 then
+        state.max = math.max(state.max, number)
+		if dt > target then
+			number = number - 5
+		else
+			number = number + 30
+		end
+	end
+	tick = tick + 1
+    if tick > 600 and dt - target <= 1 then
+        print(state.name .. '        ' .. state.max)
+        current_state = current_state + 1
+        if states[current_state] then
+            tick = 0
+            number = 0
+            set_state(states[current_state])
+        else
+            perf_stop()
+        end
+    end
+end
+
+function perf_stop()
+    drystal.stop()
+end
+
+function drystal.draw()
+    state:draw()
+end
+
 function drystal.key_press(k)
 	if k == 'a' then
-		drystal.stop()
+		perf_stop()
 	end
 end
