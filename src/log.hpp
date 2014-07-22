58a3106e2556b075ad5d400a5e43906e3e906f8b
Ronny Chevalier <chevalier.ronny@gmail.com>
2014-07-22 22:23:06 +0200

    log: add new logging macros

--- src/log.hpp
+++ src/log.hpp
@@ -16,13 +16,47 @@
  */
 #pragma once
 
+#include <cstdio>
+
+#include "macro.hpp"
+
+/*
+ * Before using the logging API you must call log_category() at the beginning
+ * of your file, like this:
+ * log_category("display");
+ * [...]
+ * if (r < 0) {
+ *     log_error("an error happenned"");
+ * }
+ */
+#define log_category(cat) \
+	static _unused_ const char *_log_category = cat
+
+#define log_message(level, msg, ...) \
+	fprintf(stderr, "[%4s|%10s] " msg "\n", level, _log_category, ##__VA_ARGS__)
+
+#ifdef DODEBUG
+#define log_debug(msg, ...) \
+	log_message("DBG", "[%s:%d %s()] " msg, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define log_info(msg, ...) \
+	log_message("INFO", msg, ##__VA_ARGS__)
+#else
+#define log_debug(msg, ...)
+#define log_info(msg, ...)
+#endif
+
+#define log_warning(msg, ...) \
+	log_message("WARN", msg, ##__VA_ARGS__)
+
+#define log_error(msg, ...) \
+	log_message("ERR", msg, ##__VA_ARGS__)
+
 #ifdef DODEBUG
 #ifndef EMSCRIPTEN
 #include <SDL2/SDL_opengles2.h>
 #else
 #include <SDL/SDL_opengl.h>
 #endif
-#include <cstdio>
 
 #define DEBUG(fmt)\
 	do { fprintf(stderr, "%10.10s:%d\t%s()\t" fmt "\n", __FILE__, \
