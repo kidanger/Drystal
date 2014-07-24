bad40134e9012fb579a108121f9431d0651f7ee6
Ronny Chevalier <chevalier.ronny@gmail.com>
2014-07-24 16:44:28 +0200

    add license header

--- src/graphics/api.hpp
+++ src/graphics/api.hpp
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Drystal.
+ *
+ * Drystal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Drystal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Drystal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "module.hpp"
 #include "engine.hpp"
 #include "display_bind.hpp"
