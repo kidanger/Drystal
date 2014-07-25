1bd91ca9b924e5265d04d80b12258861f704121a
kidanger <angerj.dev@gmail.com>
2014-07-25 15:55:06 +0200

    Update livecoding.lua test

--- tests/misc/livecode.lua
+++ tests/misc/livecode.lua
@@ -1,17 +1,12 @@
 local drystal = require 'drystal'
 local l = require 'livecode2'
 
+drystal.resize(300, 200)
+
 print('value:', l.value)
 
 function drystal.init()
-	print('============')
-	if not drystal.screen then
-		drystal.resize(math.random(100, 300), math.random(100, 300))
-	end
-	print('send SIGUSR1 to drystal or press \'x\'')
-end
-
-function drystal.update()
+	print('init')
 end
 
 function drystal.key_press(k)
