3547d210851efd84b42e5ae15b2063c45cf892f2
kidanger <angerj.dev@gmail.com>
2014-07-25 15:25:36 +0200

    use --recursive for astyle and format C code sources

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,7 +1,9 @@
 add_custom_target(astyle COMMAND
     astyle --indent=tab --indent-preprocessor --style=kr
     --pad-oper --pad-header --unpad-paren -S -n
-    ${CMAKE_SOURCE_DIR}"/src/*.cpp"
+    --recursive ${CMAKE_SOURCE_DIR}"/src/*.cpp"
     ${CMAKE_SOURCE_DIR}"/src/*.hpp"
+    ${CMAKE_SOURCE_DIR}"/src/*.c"
+    ${CMAKE_SOURCE_DIR}"/src/*.h"
 )
 
