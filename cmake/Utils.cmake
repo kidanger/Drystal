59ee904ab73e921ac4e633f68d9708524df60777
Ronny Chevalier <chevalier.ronny@gmail.com>
2014-07-25 15:38:42 +0200

    astyle: exclude api.hpp

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,6 +1,7 @@
 add_custom_target(astyle COMMAND
     astyle --indent=tab --indent-preprocessor --style=kr
     --pad-oper --pad-header --unpad-paren -S -n
+    --exclude="api.hpp"
     --recursive ${CMAKE_SOURCE_DIR}"/src/*.cpp"
     ${CMAKE_SOURCE_DIR}"/src/*.hpp"
     ${CMAKE_SOURCE_DIR}"/src/*.c"
