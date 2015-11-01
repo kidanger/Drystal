b48203eb3bebb156d1e702eb82d313e84e29617b
kidanger <angerj.dev@gmail.com>
2015-11-01 21:01:47 +0100

    spec: close the window after running the tests

--- tools/unittest.lua
+++ tools/unittest.lua
@@ -3,13 +3,13 @@
 package.path = './.rocks/share/lua/5.3/?.lua;./.rocks/share/lua/5.3/?/init.lua;' .. package.path
 package.cpath = './.rocks/lib/lua/5.3/?.so;' .. package.cpath
 
-require "moonscript"
+require 'moonscript'
 require 'spec.tools'
-drystal = require 'drystal'
-os.exit = drystal.stop
 
 require 'busted.runner' {
 	standalone=false,
-	batch=true,
 }
+
+drystal = require 'drystal'
+drystal.stop()
 
